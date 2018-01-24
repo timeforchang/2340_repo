256a866645f4f37765718898279b58b18d568316
chang.andrew.23@gmail.com <andrew_chang@lawn-143-215-52-105.lawn.gatech.edu>
2018-01-24 18:54:35 -0500

    first implementation of method without testing

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -5,7 +5,7 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  modified string 
  *  
- *  @author Bob
+ *  @author Andrew Chang
  *  @version 1.1
  */
 public class Person1 {
@@ -31,7 +31,13 @@ public class Person1 {
 	 */
 	private String calc(String input) {
 	  //Person 1 put your implementation here
-	  return null;
+	  if(input.length() < 3) {
+	  	return input;
+	  } else {
+	  	String newString = input.substring(2);
+	  	String suffix = input.substring(0, 2);
+	  	return newString + suffix;
+	  }
 	}
 	
 	/**
