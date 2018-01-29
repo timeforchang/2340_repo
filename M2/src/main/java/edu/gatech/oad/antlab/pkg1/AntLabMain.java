1bcf04651d80ab9275ce671ff1fc9cccaa3eef17
Yuri <xgu79@gatech.edu>
2018-01-28 19:40:13 -0500

    All team members' names included in AntLabMain.java

--- M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -74,8 +74,8 @@ import edu.gatech.oad.antlab.pkg2.*;
         toPrint += p4.toString("yhuang481");
         //Person5 replace P5 with your name
         //and gburdell5 with your gt id
-        Person5 p5 = new Person5("P5");
-        toPrint += p5.toString("gburdell5");
+        Person5 p5 = new Person5("Xiaoyu Gu");
+        toPrint += p5.toString("xgu79");
         //Person6 replace P6 with your name
         //and gburdell6 with your gt id
         Person6 p6 = new Person6("Zhuo Jiang");
